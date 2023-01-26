b56a72b7c658e65c83f109a92f1f88adb3917bb0
adya-mahajan <adya@live.unc.edu>
2023-01-26 16:21:50 -0500

    const server arrow function.

--- server.js
+++ server.js
@@ -45,8 +45,11 @@ fs.readFile('./public/index.html', 'utf8', (err,data) => {
 // 2. set a header with content type `text/html`, and 
 // 3. end with the data that you are reading in from ./public/index.html.
 
-
-
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content Type', 'text/html');
+    res.end('./public/index/html')
+});
 
 
 // Start the `server` const listening on the port defined by argument in your `port` const. 
