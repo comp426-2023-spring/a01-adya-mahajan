a0eb2c95f05cf8bc1e3da5534deb4f7b893d17a1
adya-mahajan <adya@live.unc.edu>
2023-04-09 15:39:52 -0400

    debug

--- server.js
+++ server.js
@@ -45,8 +45,8 @@ fs.readFile('./public/index.html', 'utf8', (err,data) => {
 
     const server = http.createServer((req, res) => {
         res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/html');
-        res.end('./public/index/html')
+        res.setHeader(`Content-Type`, `text/html`);
+        res.end(data)
     })
 
 
@@ -54,8 +54,8 @@ fs.readFile('./public/index.html', 'utf8', (err,data) => {
 // Put the exact message `Server listening on port ${port}` on the console log. 
 
     server.listen(port, () => {
-        console.log('Server listening on port ${port}');
-    });
+        console.log(`Server listening on port ${port}`);
+    })
 
 });
 // That's it! You're all done!
